5211973e52f0e3e67db3fee155eb10bb3501622c
moluo <jinz13981719915@163.com>
2023-05-11 21:41:01 +0800

    Initial commit

--- Core/BSP/motor.c
+++ Core/BSP/motor.c
@@ -1,3 +1,45 @@
 //
 // Created by ShiF on 2023/5/9.
 //
+#include "bsp_headfile.h"
+#include "motor.h"
+
+float update_Des(float set)         //位置环参数转换
+{
+    return set;
+}
+
+/*******自转********/
+/*  三个轮子同向转动 */
+/******************/
+void CAR_spin(float des)
+{
+    SetPos_F+=des;
+    SetPos_BR+=des;
+    SetPos_BL+=des;
+}
+
+/*******纵向********/
+/*   前轮作从动轮   */
+/*左后轮和右后轮作驱动轮*/
+/******************/
+void CAR_longitudinal(float des)
+{
+    SetPos_F+=0;
+    SetPos_BL+=-des/((float)COS_30);
+    SetPos_BR+=des/((float)COS_30);
+
+}
+/*******横向********/
+/*三个轮子同时作为驱动轮*/
+/*前轮为主导方向的轮子*/
+/*后两轮转向相同，且与前轮转向相反*/
+/*Des为正时向左平移*/
+/******************/
+void CAR_transverse(float des)
+{
+    SetPos_F+=-des;
+    SetPos_BL+=des/((float)COS_60);
+    SetPos_BR+=des/((float)COS_60);
+
+}
--- Core/BSP/motor.h
+++ Core/BSP/motor.h
@@ -5,4 +5,12 @@
 #ifndef EE_CODE_MOTOR_H
 #define EE_CODE_MOTOR_H
 
+#define COS_30  0.866
+#define COS_60  0.500
+
+float   update_Des(float set);
+void    CAR_spin(float des);
+void    CAR_longitudinal(float des);
+void    CAR_transverse(float des);
+
 #endif //EE_CODE_MOTOR_H
